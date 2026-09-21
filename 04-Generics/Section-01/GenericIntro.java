aa8eae051b861b747daec52ddc5bc24f53e04e90
Clarissa Milligan <milligan.s.clarissa@gmail.com>
2026-09-21 12:09:52 -0400

    generics practice

--- 04-Generics/Section-01/GenericIntro.java
+++ 04-Generics/Section-01/GenericIntro.java
@@ -14,5 +14,35 @@ public class GenericIntro
         for (int i = 0; i < roster.size(); i++) {
             //roster.get(i).toUpperCase();
         }
+
+        Randomizer<Integer> lotteryDraw = new Randomizer<>();
+
+        for (int i = 1; i <= 100; i++)
+        {
+            lotteryDraw.addItem(i);
+        }
+
+        System.out.println(lotteryDraw.pickItem());
+        System.out.println(lotteryDraw.pickItem());
+        System.out.println(lotteryDraw.pickItem());
+        System.out.println(lotteryDraw.pickItem());
+        System.out.println(lotteryDraw.pickItem());
+        lotteryDraw.displayAllContents();
+
+
+        Randomizer<Character> classRoster = new Randomizer<>();
+
+        for (int i = 97; i < 123; i++)
+        {
+            Character letter = (char) i;
+            classRoster.addItem(letter);
+        }
+
+        System.out.println(classRoster.pickItemDestructive());
+        System.out.println(classRoster.pickItemDestructive());
+        System.out.println(classRoster.pickItemDestructive());
+        System.out.println(classRoster.pickItemDestructive());
+        System.out.println(classRoster.pickItemDestructive());
+        classRoster.displayAllContents();
     }
 }
--- 04-Generics/Section-01/Randomizer.java
+++ 04-Generics/Section-01/Randomizer.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Randomizer<E>
+public class Randomizer<E extends Comparable<E>> implements Comparable<Randomizer<E>>
 {
     private ArrayList<E> contents = new ArrayList<>();
     private Random rng = new Random();
@@ -12,20 +12,42 @@ public class Randomizer<E>
 
     public Randomizer(ArrayList<E> items)
     {
-        for (E item : items)
-        {
-            //contents.add(new E());
-        }
-        //contents.addAll(items);
+//        for (E item : items)
+//        {
+//            contents.add(item);
+//        }
+        // shallow copies items
+        // can't easily get around this with generics
+        // no guarantee of a copy constructor, no-arg constructor, clone(), etc.
+        contents.addAll(items);
     }
 
     public void addItem(E item)
     {
-
+        contents.add(item);
     }
 
-//    public E pickItem()
-//    {
-//
-//    }
+    public E pickItem()
+    {
+        int randomIndex = rng.nextInt(contents.size());
+        return contents.get(randomIndex);
+    }
+
+    public E pickItemDestructive()
+    {
+        int randomIndex = rng.nextInt(contents.size());
+        return contents.remove(randomIndex);
+    }
+
+    public void displayAllContents()
+    {
+        Collections.sort(contents);
+        System.out.println(contents);
+    }
+
+    @Override
+    public int compareTo(Randomizer<E> o)
+    {
+        return 0;
+    }
 }
